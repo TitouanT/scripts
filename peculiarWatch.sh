824a9c8838bd7347212893bc4ca1326b4393a8bf
TitouanT <titouan.teyssier@gmail.com>
2019-06-04 16:37:52 +0200

    explaination

--- peculiarWatch.sh
+++ peculiarWatch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 last=$(peculiarLast.sh)
+# to update the script move on the line below and hit `0llD@"`
+# p:r!peculiarLast.shD3f"vi"pkdd
 if [ "$last" != "A Map of Days" ]
 then
 	notify-send -t 0 "Peculiar's last book: $last"
